4a47dac8b41a76f367a722588c64cc04d36e47da
Matías Gutiérrez <matyuh@hotmail.com>
2021-10-22 11:30:01 -0300

    added moveIntoSquareBelow function

--- src/components/board/board.component.jsx
+++ src/components/board/board.component.jsx
@@ -65,6 +65,23 @@ const Board = () => {
         }
     }
 
+    const moveIntoSquareBelow = () => {
+        for (let i = 0; i < 64 - width; i++) {
+            const firstRow = [0,1,2,3,4,5,6,7];
+            const isFirstRow = firstRow.includes(i)
+
+            if (isFirstRow && currentColorArray[i] === ''){
+                let randomNumber = Math.floor(Math.random() *candyColors.length);
+                currentColorArray[i] = candyColors[randomNumber]
+            }
+
+            if (currentColorArray[i + width] === ''){
+                currentColorArray[i + width] = currentColorArray[i];
+                currentColorArray[i] = ''
+            }
+        }
+    }
+
     const createBoard = () => {
         const randomColorArray = [];
         for (let i = 0; i < width * width; i++){
@@ -83,11 +100,12 @@ const Board = () => {
             checkForColumnOfFour()
             checkForColumnOfThree()
             checkForRowOfFour()
-            checkForRowOfThree()            
+            checkForRowOfThree()
+            moveIntoSquareBelow()            
             setCurrentColorArray([...currentColorArray])
         }, 100)    
         return () => clearInterval(timer)    
-    }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, currentColorArray])
+    }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, moveIntoSquareBelow, currentColorArray])
 
     return (
         <BoardContainer>
